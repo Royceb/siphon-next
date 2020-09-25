68eafd8b55fb58b65d5c3e18d6b9585cc7d24b7e
royceb <royce.r.brooks@gmail.com>
2020-09-24 23:22:24 -0700

    styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import Head from 'next/head';
-import Layout from '../components/layout';
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Layout from "../components/layout";
+import "../css/style.scss";
+import AOS from "aos";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: "phone",
+      duration: 600,
+      easing: "ease-out-sine",
+    });
+  });
+
   return (
     <Layout>
       <Head>
